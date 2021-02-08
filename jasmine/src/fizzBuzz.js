0e04b9b5a359ddea006df09c0821331805c501c6
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-02-08 17:42:30 +0000

    FizzBuzz in JavaScript

---
+++ jasmine/src/fizzBuzz.js
@@ -0,0 +1,31 @@
+class Javabuzz{
+
+  says(number) {
+    if (this.isDivisibleByFifteen(number)) {
+      return 'Javabuzz';
+    }
+    if (this.isDivisibleByThree(number)) {
+      return 'Java';
+    }
+    if (this.isDivisibleByFive(number)) {
+      return 'Buzz';
+    }
+    return number;
+  }
+
+  isDivisibleByThree(number) {
+    return this._isDivisibleBy(number, 3);
+  }
+
+  isDivisibleByFive(number) {
+    return this._isDivisibleBy(number, 5);
+  }
+ 
+  isDivisibleByFifteen(number) {
+    return this._isDivisibleBy(number, 15);
+  }
+
+  _isDivisibleBy(number, divisor) {
+    return (number % divisor === 0);
+  }
+}
